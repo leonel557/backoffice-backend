d268861960d94d757aea5dae457492b790da4444
Leonel <leo.lima.user08@gmail.com>
2022-08-27 10:22:10 -0300

    Questions and answers

---
+++ sql/fill_tables.sql
@@ -0,0 +1,59 @@
+INSERT INTO questions (description) VALUES ('Which of the following frameworks is a React framework?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (1, 'Laravel', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (1, 'NextJS', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (1, 'Spring', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (1, 'Django', false);
+
+INSERT INTO questions (description) VALUES ('What is useState hook used for?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (2, 'To submit a form', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (2, 'To detect changes on the component state', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (2, 'To save a component state', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (2, 'To make an API request', false);
+
+INSERT INTO questions (description) VALUES ('Which of the following databases is a NoSQL dabase?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (3, 'MongoDB', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (3, 'MySQL', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (3, 'Postgres', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (3, 'Oracle Database', false);
+
+INSERT INTO questions (description) VALUES ('What is Server Side Rendering (SSR)?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (4, 'A programming language', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (4, 'A way to make requests to an API', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (4, 'A database management system', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (4, 'A methodology to render a website from server', true);
+
+INSERT INTO questions (description) VALUES ('Which of the following options is a CSS framework?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (5, 'Typescript', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (5, 'Atomic design', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (5, 'Tailwind', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (5, 'CORS', false);
+
+INSERT INTO questions (description) VALUES ('How we can create a responsive design with CSS?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (6, 'Using the CSS @media rule', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (6, 'Using React', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (6, 'Using the CSS @import rule', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (6, 'Using SaSS', false);
+
+INSERT INTO questions (description) VALUES ('What is React?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (7, 'A Javascript client library', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (7, 'A Typescript template to initialize a project', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (7, 'A backend programming language', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (7, 'A programming methodology', false);
+
+INSERT INTO questions (description) VALUES ('What is Visual Studio Code used for?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (8, 'To code projects in any language you want', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (8, 'To debug Javascript codes', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (8, 'To create React and Node templates', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (8, 'To connect to different databases', false);
+
+INSERT INTO questions (description) VALUES ('What is NodeJs?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (9, 'It is a Python framework', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (9, 'It is a Javascript version for backend', true);
+INSERT INTO answers (id_question, description, is_correct) VALUES (9, 'It is a Java Framework', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (9, 'It is a database based on Javascript', false);
+
+INSERT INTO questions (description) VALUES ('Which of the following is not a OOP language?');
+INSERT INTO answers (id_question, description, is_correct) VALUES (10, 'Javascript', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (10, 'C#', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (10, 'Java', false);
+INSERT INTO answers (id_question, description, is_correct) VALUES (10, 'C++', true);
